47e7e9626eb6b6415a36c9bdd4bff3d4bf9f2d32
kasvi19 <133293826+kasvi19@users.noreply.github.com>
2023-07-08 16:46:13 +0530

    DNA string manipulation.py
    As part of the Algorithms for Genomic Data Science Course on Coursera

--- DNA string manipulation.py
+++ DNA string manipulation.py
@@ -1,4 +1,6 @@
 import random
+
+# Creating a random sequence of 10 bp:
 def RANDOM(s1):
     seq=""
     seq="".join([random.choice("ATGC") for _ in range(10)])
@@ -7,6 +9,7 @@ def RANDOM(s1):
 x=RANDOM("")
 y=RANDOM("")
 
+# Checking whether the two sequences are identical:
 def match(s1,s2):
     if not len(s1)==len(s2):
         return False
@@ -14,20 +17,22 @@ def match(s1,s2):
         if not s1[i]==s2[i]:
             return False
     return True
-
+    
+# Function to find the longest common suffix:
 def LCS(s1,s2):
     i=0
     while i<len(s1) and i<len(s2) and s1[-i]==s2[-i]:
         i+=1
     print(s1[-i:])
 
-
+# Function to find the longest common prefix:
 def LCP(s1,s2):
     i=0
     while i<len(s1) and i<len(s2) and s1[i]==s2[i]:
         i+=1
     print(s1[:i])
-
+    
+# Function to find the Reverse Complement of the string:
 def RevCom(s):
     complement = {"A": "T", "G": "C", "T": "A", "C": "G"}
     t = ''
